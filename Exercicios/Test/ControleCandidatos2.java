a6ce1771d5dcdfd9cde27aff8598701b8467f055
Emanuel <emanuel.rudek@gmail.com>
2023-08-29 20:58:59 -0300

    Selecionando Candidatos

--- Exercicios/Test/ControleCandidatos2.java
+++ Exercicios/Test/ControleCandidatos2.java
@@ -5,10 +5,30 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ControleCandidatos2 {
     public static void main(String[] args) {
-        analiseCandidato(2300.0);
-        analiseCandidato(2000.0);
-        analiseCandidato(1900.0);
+        processoSeletivo();
+    }
+
+
+    static void processoSeletivo() {
+        String[] candidatos = {"Aline", "Alceu", "Bruno", "Barbara", "Bruna", "Claudio", "Cesar", "Ciro", "Clara", "Diana", "Dario", "Dirceu", "Eliana"};
+
+        int candidatosSelecionado = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionado < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double valorVaga = salarioPretendido();
+            System.out.println("Candidato" + candidato + ", Requiriu um salário de: " + valorVaga);
+            if (valorVaga <= salarioBase) {
+                System.out.println(candidato + " candidato(a) contratado(a)");
+                candidatosSelecionado++;
+            }
+            candidatosAtual++;
         }
+        if (candidatosSelecionado < 5) {
+            System.out.println("candidatos exigindo mais que o proposto, realizar novas entrevistas");
+        }
+    }
 
     static double salarioPretendido() {
         return ThreadLocalRandom.current().nextDouble(1800, 2200);
@@ -20,6 +40,8 @@ public class ControleCandidatos2 {
         System.out.println(respostaCandidato);
     }
 }
+
+
 
 
 
